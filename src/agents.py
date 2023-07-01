24ce8253bd4d7cb95373cd3455c090dd5a5fcbaf
Antonio Igor Carvalho <antonioigorcarvalho@gmail.com>
2023-07-01 16:50:25 -0300

    Novos Agentes Criados
    Co-authored-by: Daniel Porto <danielportods@gmail.com>
    Co-authored-by: João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
    Co-Authored-by: Mateus Moreira <mateuuusss@gmail.com>
    Co-authored-by: KessJhones <tavaresjhones14@gmail.com>
    Co-Authored-by: Yan Andrade <yan13052001@gmail.com>
    Co-authored-by: vitordiniz25 <vitordiniz25@gmail.com>
    Co-Authored-by: yuriAlves5 <yurivalerio12@gmail.com>

--- src/agents.py
+++ src/agents.py
@@ -4,3 +4,22 @@ class CellAgent(Agent):
     def __init__(self, unique_id, model, agent_type):
         super().__init__(unique_id, model)
         self.type = agent_type
+
+    def step(self):
+        print("Hi, I am agent " + str(self.unique_id) + "type: " + str(self.type))
+
+class HeroAgent(Agent):
+    def __init__(self, unique_id, model, pos):
+        super().__init__(unique_id, model)
+        self.pos = pos
+
+    def step(self):
+        print("Hi, I am agent " + str(self.unique_id) + "type: hero position: " + str(self.pos))
+
+class EnemyAgent(Agent):
+    def __init__(self, unique_id, model, pos):
+        super().__init__(unique_id, model)
+        self.pos = pos
+
+    def step(self):
+        print("Hi, I am agent " + str(self.unique_id) + "type: enemy position: " + str(self.pos))
--- src/model.py
+++ src/model.py
@@ -9,6 +9,9 @@ class bomberdaModel(Model):
 
     def __init__(self):
         self.current_id = 0
+        self.has_enemy = False
+        self.has_treasure = False
+        
         mapa = b.getMap()
         splited_map = mapa.split('\n')
         splited_map.remove('')
@@ -25,11 +28,22 @@ class bomberdaModel(Model):
                 symbol = splited_map[row][col]
                 
                 if symbol == '@':
-                    pass
-                    # Adicionar o personagem
+                    new_hero = HeroAgent(self.next_id(), self, (row, col))
+                    self.grid.place_agent(new_hero, (row, col))
+                    self.schedule.add(new_hero)
                 elif symbol == 'E':
-                    pass
-                    # Adicionar o inimigo
+                    self.has_enemy = True
+                    new_enemy = EnemyAgent(self.next_id(), self, (row, col))
+                    self.grid.place_agent(new_enemy, (row, col))
+                    self.schedule.add(new_enemy)
+                elif symbol == '.':
+                    self.has_treasure = True
 
                 new_agent = CellAgent(self.next_id(), self, symbol)
                 self.grid.place_agent(new_agent, (row, col))
+                self.schedule.add(new_agent)
+                
+    def step(self):
+
+        """Advance the model by one step."""
+        self.schedule.step()
